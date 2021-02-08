0fdccb6dd4116cb5e1eaee39f8f584e28adf7004
Zac Apelt <22apeltz@marash.qld.edu.au>
2021-02-08 20:54:19 +1000

    initial commit

---
+++ pygameproject.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame.locals import *
+import os, sys
+
+pygame.init()
+pygame.mixer.init()
+pygame.font.init()
+
+dimensions = [1024, 600]
+#pygame.display.set_mode[dimensions]
+pygame.display.set_caption('myGame')
+screen = pygame.display.get_surface()
+screen.fill((0,255,0))
+
+input('press enter to exit')
+pygame.quit()
+sys.exit(0)
