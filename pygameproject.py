9fd48d98ec7eb81acb709842178546be9405daf3
Zac Apelt <22apeltz@marash.qld.edu.au>
2021-02-09 07:56:51 +1000

    l

--- pygameproject.py
+++ pygameproject.py
@@ -5,12 +5,13 @@ import os, sys
 pygame.init()
 pygame.mixer.init()
 pygame.font.init()
- 
+  
 dimensions = [1024, 600]
-pygame.display.set_mode[dimensions]
+pygame.display.set_mode(dimensions)
 pygame.display.set_caption('myGame')
 screen = pygame.display.get_surface()
 screen.fill((0,255,0))
+pygame.display.flip()
 
 input('press enter to exit')
 pygame.quit()
