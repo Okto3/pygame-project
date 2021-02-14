2090a059439e4092dd67ef6dfc136574e3bb357e
Zac Apelt <22apeltz@marash.qld.edu.au>
2021-02-14 21:51:27 +1000

    commit bouncing ball and dragging rectangle

--- platform.py
+++ platform.py
@@ -2,31 +2,46 @@ import pygame
 
 class platform(pygame.sprite.Sprite):
     def __init__ (self):
-        rectangle = pygame.rect.Rect(176, 134, 30, 30)
-        rectangle_draging = False
+        pygame.sprite.Sprite.__init__(self)
+        self.rectangle = pygame.rect.Rect(176, 134, 30, 30)
+        self.rectangle_draging = False
+        self.surface = pygame.display.get_surface()
+        self.offset_x = 0
+        self.offset_y = 0
+
+        
+        
+
+    def drag (self):
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
 
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:            
-                    if rectangle.collidepoint(event.pos):
-                        rectangle_draging = True
+                    if self.rectangle.collidepoint(event.pos):
+                        self.rectangle_draging = True
                         mouse_x, mouse_y = event.pos
-                        offset_x = rectangle.x - mouse_x
-                        offset_y = rectangle.y - mouse_y
+                        self.offset_x = self.rectangle.x - mouse_x
+                        self.offset_y = self.rectangle.y - mouse_y
 
             elif event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:            
-                    rectangle_draging = False
+                    self.rectangle_draging = False
 
             elif event.type == pygame.MOUSEMOTION:
-                if rectangle_draging:
+                if self.rectangle_draging:
                     mouse_x, mouse_y = event.pos
-                    rectangle.x = mouse_x + offset_x
-                    rectangle.y = mouse_y + offset_y
+                    self.rectangle.x = mouse_x + self.offset_x
+                    self.rectangle.y = mouse_y + self.offset_y
+                    #self.move()
 
-    pygame.draw.rect(screen, RED, rectangle)
+    def move (self):
+            
+            self.drawRectangle()
+            #self.surface.blit(self.surface, (self.rectangle.x, self.rectangle.y))
+    
+    def drawRectangle (self):
+        self.update()
+        pygame.draw.rect(self.surface, (0,255,0), self.rectangle)
 
  
